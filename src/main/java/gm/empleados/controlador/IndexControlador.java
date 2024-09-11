c2f7b98d41b8fec501f36093533c46217647ab1a
Adrián Benítez Roldán <149155965+AdrianBR003@users.noreply.github.com>
2024-09-11 15:10:56 +0200

    fix: IndexControlador.java
    Error description: org.springframework.web.servlet.resource.ResourceHttpRequestHandler - "Path with "WEB-INF" or "META-INF": [webapp/WEB-INF/jsp/index.jsp]"
    
    Solution:
    
    Add the ViewResolver
    
    *Maybe it happened because of the JDK version

---
+++ src/main/java/gm/empleados/controlador/IndexControlador.java
@@ -0,0 +1,33 @@
+package gm.empleados.controlador;
+
+import gm.empleados.modelo.Empleado;
+import gm.empleados.servicio.EmpleadoServicio;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+
+@Controller
+public class IndexControlador { // Controlador de la pagina de inicio
+    private static final Logger logger =
+            LoggerFactory.getLogger(IndexControlador.class); // Mandamos la información de esta clase a la consola
+
+    @Autowired // Trabajar con la capa de servicio
+    EmpleadoServicio empleadoServicio;
+
+    // Va a informar del archivo JSP que vamos a utilizar
+    @RequestMapping(value="/", method = RequestMethod.GET) // Especificamos la URL que vamos a utilizar
+    public String iniciar(ModelMap modelo){
+        List<Empleado> empleados = empleadoServicio.listarEmpleados();
+        empleados.forEach((empleado -> logger.info(empleado.toString()))); // enviamos los empleados de la lista por consola
+        // Compartimos el modelo con la vista
+        modelo.put("empleados", empleados); // Especificamos el 'id' y luego la información que sería la variable
+        return "index"; // index.jsp
+    }
+}
